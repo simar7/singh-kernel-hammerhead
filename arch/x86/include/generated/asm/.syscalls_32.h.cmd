054d8f8e052c277fac82ee2692e2cee1395d100c
Simarpreet Singh <simar@linux.com>
2014-02-12 21:22:26 +0000

    Lowering down default brightness level.
    Signed-off-by: Simarpreet Singh <simar@linux.com>

---
+++ arch/x86/include/generated/asm/.syscalls_32.h.cmd
@@ -0,0 +1,1 @@
+cmd_arch/x86/syscalls/../include/generated/asm/syscalls_32.h := /bin/bash '/home/simar/Development/singh-kernel/arch/x86/syscalls/syscalltbl.sh' /home/simar/Development/singh-kernel/arch/x86/syscalls/syscall_32.tbl arch/x86/syscalls/../include/generated/asm/syscalls_32.h
---
+++ arch/x86/include/generated/asm/.unistd_32.h.cmd
@@ -0,0 +1,1 @@
+cmd_arch/x86/syscalls/../include/generated/asm/unistd_32.h := /bin/bash '/home/simar/Development/singh-kernel/arch/x86/syscalls/syscallhdr.sh' '/home/simar/Development/singh-kernel/arch/x86/syscalls/syscall_32.tbl' 'arch/x86/syscalls/../include/generated/asm/unistd_32.h' 'i386' '' ''
