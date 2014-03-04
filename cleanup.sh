536430be1daad33563b72028461e076877adf53a
Simarpreet Singh <simar@linux.com>
2014-03-04 00:49:55 +0000

    scripts: Fixing some bugs and hello Travis CI
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ make clean
 make mrproper
 
 # also remove any deleted files, if any
-if [ -z `git ls-files --deleted` ]
+if [ `git ls-files --deleted` ]
 then
 	git rm $(git ls-files --deleted) 
 fi
