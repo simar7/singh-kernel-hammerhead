26c412bb261023596f0dd86487a1dfadd901c558
Simarpreet Singh <simar@linux.com>
2014-03-04 06:08:44 +0000

    Adding debug flags for travis
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- make_kernel.sh
+++ make_kernel.sh
@@ -2,6 +2,7 @@
 
 DEFCONFIG_FILE=$1
 CROSS_COMPILE=$2
+DEBUG=$3
 
 if [ -z "$DEFCONFIG_FILE" ]; then
 	echo "Need defconfig file(hammerhead_defconfig)!"
@@ -17,18 +18,21 @@ fi
 env KCONFIG_NOTIMESTAMP=true \
 make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE}
 
-# run menuconfig
-env KCONFIG_NOTIMESTAMP=true \
-make menuconfig ARCH=arm
+if [ -n "$DEBUG" ]; then
+	# run menuconfig for custom config
+	env KCONFIG_NOTIMESTAMP=true \
+	make menuconfig ARCH=arm
+fi
 
 make savedefconfig ARCH=arm
+
 # copy .config to defconfig
 mv defconfig arch/arm/configs/${DEFCONFIG_FILE}
+
 # clean kernel object
 make mrproper
 
 # build kernel image
 make -j16 hammerhead_defconfig ARCH=arm SUBARCH=arm CROSS_COMPILE=${CROSS_COMPILE}
-
 make -j16 ARCH=arm SUBARCH=arm CROSS_COMPILE=${CROSS_COMPILE}
 
